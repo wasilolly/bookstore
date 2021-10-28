15a45cdccb9eb9994d33976bab30b285393688b2
wasilolly <balogunw@tcd.ie>
2021-10-28 19:53:26 +0100

    Author API resource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::get('/test', function () {
+   return 'Authenticated'; 
+});
+Route::middleware('auth:api')->prefix('v1')->group( function() {
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+
+       Route::apiResource('/authors', AuthorsController::class);
+
+});
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
